82daf449bd56e248c913d833499689c8010e72c1
Albert Sare <thesundaymailman@gmail.com>
2018-07-10 11:17:46 -0700

    updated registration action to use api url as environment variable

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,20 +1,23 @@
 import {SubmissionError} from 'redux-form';
 
+import {API_BASE_URL} from '../config.js';
 import {normalizeResponseErrors} from '../actions/utils.js';
 
 export const registerUser = (username, password) => (dispatch, getState) => {
-  return fetch('http://localhost:8080/api/users', {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({username, password})
-  })
-  .then(res => normalizeResponseErrors(res))
-  .then(res => res.json())
-  .catch(err => {
-    const {reason, message, location} = err;
-    if (reason === 'ValidationError') {
-      // Convert ValidationErrors into SubmissionErrors for Redux Form
-      return Promise.reject(new SubmissionError({[location]: message}));
-    }
-  });
+  return (
+    fetch(`${API_BASE_URL}/users`, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({username, password})
+    })
+    .then(res => normalizeResponseErrors(res))
+    .then(res => res.json())
+    .catch(err => {
+      const {reason, message, location} = err;
+      if (reason === 'ValidationError') {
+        // Convert ValidationErrors into SubmissionErrors for Redux Form
+        return Promise.reject(new SubmissionError({[location]: message}));
+      }
+    })
+  );
 };
